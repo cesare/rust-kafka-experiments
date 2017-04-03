713d223ad86e1cf458d4dbddbe625b53e3eee577
SAWADA Tadashi <cesare@mayverse.jp>
2017-04-03 23:24:09 +0900

    Add minimal producer

--- src/kafka-producer.rs
+++ src/kafka-producer.rs
@@ -1,3 +1,13 @@
 extern crate kafka;
+use kafka::producer::{Producer, Record};
 
-fn main() {}
+fn create_producer() -> Producer {
+    Producer::from_hosts(vec!["localhost:9092".to_owned()])
+        .create()
+        .unwrap()
+}
+
+fn main() {
+    let mut producer = create_producer();
+    producer.send(&Record::from_value("test", "hello from rust experimental producer")).unwrap();
+}
